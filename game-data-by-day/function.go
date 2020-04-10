b9d315217983474621803e1a106a6a20fc243627
Tom Szymanski <szymanski.tom@gmail.com>
2020-04-10 11:52:35 -0600

    try using firebase admin sdk

--- game-data-by-day/function.go
+++ game-data-by-day/function.go
@@ -1,7 +1,6 @@
 package function
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -15,9 +14,9 @@ import (
 )
 
 const (
-	firebaseAccountKeyPath = ""
-	logName                = "get-game-data-by-day"
-	projectID              = "warning-track-backend"
+	databaseCollection = "game-data-by-day"
+	logName            = "get-game-data-by-day"
+	projectID          = "warning-track-backend"
 )
 
 // LogMessage is a simple struct to ensure JSON formatting in logs
@@ -42,14 +41,15 @@ func GetGameDataByDay(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// setup Firebase
-	conf := &firebase.Config{
-		DatabaseURL: "https://warning-track-backend.firebaseio.com",
-	}
-
-	// TODO: https://github.com/firebase/firebase-admin-go/blob/master/snippets/db.go
-	_, err = firebase.NewApp(ctx, conf)
+	conf := &firebase.Config{DatabaseURL: fmt.Sprintf("https://%s.firebaseio.com", projectID)}
+	app, err := firebase.NewApp(ctx, conf)
 	if err != nil {
 		lg.Log(logging.Entry{Severity: logging.Error, Payload: LogMessage{Message: fmt.Sprintf("error initializing Firebase app: %s", err)}})
+		return
+	}
+	fbClient, err := app.Database(ctx)
+	if err != nil {
+		lg.Log(logging.Entry{Severity: logging.Error, Payload: LogMessage{Message: fmt.Sprintf("error initializing Firebase client: %s", err)}})
 		return
 	}
 
@@ -91,54 +91,15 @@ func GetGameDataByDay(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Integrate with Firebase to persist data
-	httpClient := &http.Client{}
-	fbPUTUrl := firebaseURL(parsedDate)
-	b, err := json.Marshal(statsAPIScheduleResp)
+	ref := fbClient.NewRef(databaseCollection)
+	err = ref.Child(d.Date).Set(ctx, statsAPIScheduleResp)
 	if err != nil {
-		lg.Log(logging.Entry{Severity: logging.Error, Payload: LogMessage{Message: fmt.Sprintf("error trying to marshal response from statsAPI: %s", err)}})
+		lg.Log(logging.Entry{Severity: logging.Error, Payload: LogMessage{Message: fmt.Sprintf("error trying to set value in Firebase: %s", err)}})
 		return
 	}
-	data := bytes.NewBuffer(b)
-	req, err := http.NewRequest(http.MethodPut, fbPUTUrl, data)
-	if err != nil {
-		lg.Log(logging.Entry{Severity: logging.Error, Payload: LogMessage{Message: fmt.Sprintf("error preparing PUT request to Firebase: %s", err)}})
-		return
-	}
-
-	lg.Log(logging.Entry{Severity: logging.Debug, Payload: LogMessage{Message: fmt.Sprintf("making Put request to firebase: %s", fbPUTUrl)}})
-	fbResp, err := httpClient.Do(req)
-	if err != nil {
-		lg.Log(logging.Entry{Severity: logging.Error, Payload: LogMessage{Message: fmt.Sprintf("error trying to make PUT to %s: %s", fbPUTUrl, err)}})
-		return
-	}
-	lg.Log(logging.Entry{Severity: logging.Debug, Payload: LogMessage{Message: fmt.Sprintf("Put response from firebase: %+v", fbResp)}})
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(statsAPIScheduleResp)
-}
-
-// firebaseURL returns the URL for firebase for this project
-func firebaseURL(time time.Time) string {
-	firebaseNamespace := "warning-track-backend"
-	databaseCollection := "game-data-by-day"
-	month := time.Format("01")
-	day := time.Format("02")
-	year := time.Format("2006")
-	return "https://" + firebaseNamespace + ".firebaseio.com/" + databaseCollection + "/" + month + "-" + day + "-" + year + ".json"
-}
-
-// getReference
-func getReference(ctx context.Context, app *firebase.App) {
-	// Create a database client from App.
-	client, err := app.Database(ctx)
-	if err != nil {
-		log.Fatalln("Error initializing database client:", err)
-	}
-
-	// Get a database reference to our blog.
-	ref := client.NewRef("server/saving-data/fireblog")
-	// [END get_reference]
-	fmt.Println(ref.Path)
 }
 
 // statsAPIScheduleURL returns the URL for all the game schedule data for the given time
