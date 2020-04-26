0d383002eca7a736a5dc991ef884e5a220a31b8b
Tom Szymanski <szymanski.tom@gmail.com>
2020-04-26 11:08:47 -0600

    try modules and no timeout context

--- game-data-by-day/function.go
+++ game-data-by-day/function.go
@@ -1,7 +1,6 @@
 package function
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -9,12 +8,12 @@ import (
 	"net/http"
 	"time"
 
-	"cloud.google.com/go/firestore"
 	"cloud.google.com/go/logging"
 	"contrib.go.opencensus.io/exporter/stackdriver"
-	firebase "firebase.google.com/go"
 	"go.opencensus.io/trace"
+	"golang.org/x/net/context"
 
+	"github.com/unrealities/warning-track-backend/gCloud"
 	"github.com/unrealities/warning-track-backend/mlbStats"
 )
 
@@ -39,7 +38,7 @@ func GetGameDataByDay(w http.ResponseWriter, r *http.Request) {
 		duration:       60 * time.Second,
 		firebaseDomain: "firebaseio.com",
 		projectID:      "warning-track-backend",
-		version:        "v0.0.38",
+		version:        "v0.0.39",
 	}
 	log.Printf("running version: %s", gameDataByDay.version)
 
@@ -50,30 +49,29 @@ func GetGameDataByDay(w http.ResponseWriter, r *http.Request) {
 	}
 	trace.RegisterExporter(exporter)
 
-	ctx, cancel := context.WithTimeout(r.Context(), gameDataByDay.duration)
-	defer cancel()
+	ctx := context.Background()
 
-	lg, err := CloudLogger(ctx, gameDataByDay.projectID, fmt.Sprintf("get-%s", gameDataByDay.dbCollection))
+	lg, err := gCloud.CloudLogger(ctx, gameDataByDay.projectID, fmt.Sprintf("get-%s", gameDataByDay.dbCollection))
 	if err != nil {
 		log.Fatalf("error setting up Google Cloud logger")
 	}
 	gameDataByDay.logger = lg
 
-	collection, err := FireStoreCollection(ctx, gameDataByDay.dbCollection, gameDataByDay.firebaseDomain, gameDataByDay.projectID, gameDataByDay.logger)
+	collection, err := gCloud.FireStoreCollection(ctx, gameDataByDay.dbCollection, gameDataByDay.firebaseDomain, gameDataByDay.projectID, gameDataByDay.logger)
 	if err != nil {
-		lg.Log(logging.Entry{Severity: logging.Error, Payload: LogMessage{Message: fmt.Sprintf("error setting up connection to FireStore: %s", err)}})
+		lg.Log(logging.Entry{Severity: logging.Error, Payload: gCloud.LogMessage{Message: fmt.Sprintf("error setting up connection to FireStore: %s", err)}})
 		return
 	}
 
 	date, err := parseDate(r.Body, gameDataByDay.dateFmt)
 	if err != nil {
-		lg.Log(logging.Entry{Severity: logging.Error, Payload: LogMessage{Message: fmt.Sprintf("error parsing date requested: %s", err)}})
+		lg.Log(logging.Entry{Severity: logging.Error, Payload: gCloud.LogMessage{Message: fmt.Sprintf("error parsing date requested: %s", err)}})
 		return
 	}
 
 	daySchedule, err := mlbStats.GetSchedule(date, gameDataByDay.logger)
 	if err != nil {
-		lg.Log(logging.Entry{Severity: logging.Error, Payload: LogMessage{Message: fmt.Sprintf("error getting the daily StatsAPI schedule: %s", err)}})
+		lg.Log(logging.Entry{Severity: logging.Error, Payload: gCloud.LogMessage{Message: fmt.Sprintf("error getting the daily StatsAPI schedule: %s", err)}})
 		return
 	}
 
@@ -86,7 +84,7 @@ func GetGameDataByDay(w http.ResponseWriter, r *http.Request) {
 	log.Printf("received response from setting the collection")
 	if err != nil {
 		log.Fatalf("error persisting data to firestore: %s", err)
-		lg.Log(logging.Entry{Severity: logging.Error, Payload: LogMessage{Message: fmt.Sprintf("error trying to set value in Firebase: %s", err)}})
+		lg.Log(logging.Entry{Severity: logging.Error, Payload: gCloud.LogMessage{Message: fmt.Sprintf("error trying to set value in Firebase: %s", err)}})
 		return
 	}
 
@@ -106,34 +104,4 @@ func parseDate(reqBody io.ReadCloser, dateFormat string) (time.Time, error) {
 	}
 
 	return time.Parse(dateFormat, d.Date)
-}
-
-// taken from gCloud
-
-// LogMessage is a simple struct to ensure JSON formatting in logs
-type LogMessage struct {
-	Message string
-}
-
-// CloudLogger sets up a connection to Google Cloud Logging for the funciton
-func CloudLogger(ctx context.Context, projectID, logName string) (*logging.Logger, error) {
-	client, err := logging.NewClient(ctx, projectID)
-	defer client.Close()
-	return client.Logger(logName), err
-}
-
-// FireStoreCollection sets up a connetion to Firebase and fetches a connection to the desired FireStore collection
-func FireStoreCollection(ctx context.Context, databaseCollection, firebaseDomain, projectID string, lg *logging.Logger) (*firestore.CollectionRef, error) {
-	conf := &firebase.Config{DatabaseURL: fmt.Sprintf("https://%s.%s", projectID, firebaseDomain)}
-	app, err := firebase.NewApp(ctx, conf)
-	if err != nil {
-		lg.Log(logging.Entry{Severity: logging.Error, Payload: LogMessage{Message: fmt.Sprintf("error initializing Firebase app: %s", err)}})
-		return nil, err
-	}
-	fsClient, err := app.Firestore(ctx)
-	if err != nil {
-		lg.Log(logging.Entry{Severity: logging.Error, Payload: LogMessage{Message: fmt.Sprintf("error initializing FireStore client: %s", err)}})
-		return nil, err
-	}
-	return fsClient.Collection(databaseCollection), nil
 }
