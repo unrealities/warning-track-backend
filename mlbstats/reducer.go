e862a4b92d47cf452f148f1b16c52386c825a66d
Tom Szymanski <szymanski.tom@gmail.com>
2020-05-06 11:29:04 -0600

    change to a reducer and return desired date

---
+++ mlbstats/reducer.go
@@ -0,0 +1,22 @@
+package mlbstats
+
+import (
+	"fmt"
+	"time"
+)
+
+// Date validates if a Schedule has a given date and returns a DateData object if it exists
+func (s Schedule) Date(date time.Time, dateFmt string) (DateData, error) {
+	for _, d := range s.Dates {
+		dateString := d.Date
+		dateTime, err := time.Parse(dateFmt, dateString)
+		if err != nil {
+			continue
+		}
+		if dateTime == date {
+			return dateTime, nil
+		}
+	}
+
+	return DateData{}, fmt.Errorf("unable to find a matching date from mlbstats")
+}
--- transformers/main.go
+++ transformers/main.go
@@ -1,7 +1,6 @@
 package transformers
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/unrealities/warning-track-backend/mlbstats"
@@ -9,12 +8,9 @@ import (
 
 // OptimusPrime takes a mlbstats.Schedule and produces an AllSpark with a day's game data
 func OptimusPrime(date time.Time, daySchedule mlbstats.Schedule) (AllSpark, error) {
-	validDate, err := daySchedule.ValidDate(date, "2020-02-01")
+	dateDate, err := daySchedule.ValidDate(date, "2020-02-01")
 	if err != nil {
 		return AllSpark{}, err
-	}
-	if !validDate {
-		return AllSpark{}, fmt.Errorf("invalid date (%v) for given daySchedule", date)
 	}
 
 	return AllSpark{}, nil
