bb246c902ee3a130ca19110d0e178c3ae3be0c3d
Tom Szymanski <szymanski.tom@gmail.com>
2024-03-24 21:16:03 -0500

    trying different date validation

--- mlbstats/reducer.go
+++ mlbstats/reducer.go
@@ -7,21 +7,19 @@ import (
 
 // Date validates if a Schedule has a given date and returns a DateData object if it exists
 func (s Schedule) Date(date time.Time) (DateData, error) {
-	var parseError error
 	for _, d := range s.Dates {
 		dateString := d.Date
 		dateTime, err := time.Parse("2006-01-02", dateString)
 		if err != nil {
-			parseError = err
 			continue
 		}
 		// Check the days are equal
-		if dateTime.Truncate(24 * time.Hour).Equal(date.Truncate(24 * time.Hour)) {
+		if dateTime.Year() == date.Year() && dateTime.YearDay() == date.YearDay() {
 			return d, nil
 		}
 	}
 
-	return DateData{}, fmt.Errorf("unable to find a matching date from mlbstats: Looking for %v. Received %v. Error: %v", date, s.Dates[0].Date, parseError)
+	return DateData{}, fmt.Errorf("unable to find a matching date from mlbstats: Looking for %v. Received %v.", date, s.Dates[0].Date)
 }
 
 // InProgress returns a bool given a game's current state if the game is in progress or not
