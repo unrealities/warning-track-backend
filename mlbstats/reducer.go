5d1eebe692dd1dc669443cfb1cb40a2aca863281
Tom Szymanski <szymanski.tom@gmail.com>
2024-03-27 10:02:51 -0500

    handle empty dates gracefully

--- mlbstats/reducer.go
+++ mlbstats/reducer.go
@@ -7,6 +7,10 @@ import (
 
 // Date validates if a Schedule has a given date and returns a DateData object if it exists
 func (s Schedule) Date(date time.Time) (DateData, error) {
+	if s.Dates == nil {
+		return DateData{}, fmt.Errorf("there are no dates in the schedule")
+	}
+
 	for _, d := range s.Dates {
 		dateString := d.Date
 		dateTime, err := time.Parse("2006-01-02", dateString)
@@ -19,7 +23,7 @@ func (s Schedule) Date(date time.Time) (DateData, error) {
 		}
 	}
 
-	return DateData{}, fmt.Errorf("unable to find a matching date from mlbstats: Looking for %v. Received %v.", date, s.Dates[0].Date)
+	return DateData{}, fmt.Errorf("unable to find a matching date from mlbstats: looking for %v. Received %v", date, s.Dates[0].Date)
 }
 
 // InProgress returns a bool given a game's current state if the game is in progress or not
